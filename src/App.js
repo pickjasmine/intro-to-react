1b5e7e1fa14f96ea3037a26c5581f5daabc65183
Jasmine Francois <francois.jasminem@gmail.com>
2021-04-26 20:32:15 -0500

    Movie Carousel with map, ternary, named and default exports, state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
+import {movies} from "./movies";
 import Button from "./Button";
 
 function App() {
-  const [counter, setCounter] = React.useState(0);
-
-  // the same as this
-  // const counterState = React.useState(0);
-  //   const counter = counterState[0];
-  //   const setCounter = counterState[1];
-
-    const increment = () => {
-        setCounter(counter + 1);
-    }
-
-    const decrement = () => {
-        setCounter(counter - 1);
-    }
+    const [currentMovie, setCurrentMovie] = useState(null);
 
   return (
    <div>
-     <p>Counter</p>
+       <h1>Movie Carousel</h1>
+       {
+           movies.map((movie) => {
+               // this is where the logic happens
+               return (
+                   <button key={movie.title} onClick={() => setCurrentMovie(movie)}>
+                       <p>{movie.title}</p>
+                       <img src={movie.posterUrl} height={250}/>
+                   </button>
+               )
+           })
+       }
 
-       <p>{counter}</p>
-
-       <Button
-           onClickFunction={increment}
-           buttonText="+"
-       />
-       <Button
-           onClickFunction={decrement}
-           buttonText="-"
-       />
+       {
+           currentMovie ?
+               <>
+                <p>{currentMovie.title}</p>
+                <p>{currentMovie.description}</p>
+                <p>{currentMovie.rating}</p>
+                <p>{currentMovie.director}</p>
+                <p>{currentMovie.releaseYear}</p>
+                <p>{currentMovie.categories.join(", ")}</p>
+               </>
+               :
+               <p>Please select a movie for details</p>
+       }
    </div>
   );
 }
